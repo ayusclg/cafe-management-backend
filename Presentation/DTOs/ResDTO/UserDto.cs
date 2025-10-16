680e7c63ff639f4cc9254f4a7f7c97995235998e
ayusclg <freelancerayus@gmail.com>
2025-10-16 16:01:42 +0545

    Login Repo service and controller Enabled

--- Presentation/DTOs/ResDTO/UserDto.cs
+++ Presentation/DTOs/ResDTO/UserDto.cs
@@ -11,8 +11,17 @@ namespace backend_01.Presentation.Response.User.Dto
             public required string UserName { get; set; }
             public required string Email { get; set; }
             [JsonConverter(typeof(JsonStringEnumConverter))]
-            public required Roles Role{ get; set; }
-            public required DateTime CreatedAt{ get; set; }
+            public required Roles Role { get; set; }
+            public required DateTime CreatedAt { get; set; }
+        }
+        
+        public class LoginUser{
+            public required int Id { get; set; }
+            public required string UserName { get; set; }
+            public required string Email { get; set; }
+            public required Roles role { get; set; }
+            
+            public required string AccessToken{ get; set; }
         }
         
     }
